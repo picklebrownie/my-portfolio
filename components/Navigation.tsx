df914b17133de3e2777dad5691234f0dec145228
Kayla <this.browniedev@gmail.com>
2023-10-04 13:03:25 -0400

    listen for scrolling for Navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
+import { socials } from '../utils/socials'
 
 const navigation = [
   { name: 'About Me', href: '#', current: true },
@@ -16,15 +17,68 @@ function classNames(...classes: any) {
   return classes.filter(Boolean).join(' ')
 }
 
+export enum ScrollDirection {
+  up = 'up',
+  down = 'down',
+}
+
 export default function Navigation() {
   const [open, setOpen] = useState(false)
+
+  const threshold = 60
+  const [scrollDir, setScrollDir] = useState(ScrollDirection.up)
+
+  useEffect(() => {
+    let previousScrollYPosition = window.scrollY
+
+    const scrolledMoreThanThreshold = (currentScrollYPosition: number) =>
+      Math.abs(currentScrollYPosition - previousScrollYPosition) > threshold
+
+    const isScrollingUp = (currentScrollYPosition: number) =>
+      currentScrollYPosition > previousScrollYPosition &&
+      !(previousScrollYPosition > 0 && currentScrollYPosition === 0) &&
+      !(currentScrollYPosition > 0 && previousScrollYPosition === 0)
+
+    const updateScrollDirection = () => {
+      console.log('open', open)
+      if (open) {
+        console.log('scroll on open', scrollDir)
+        return
+      }
+      const currentScrollYPosition = window.scrollY
+
+      if (scrolledMoreThanThreshold(currentScrollYPosition)) {
+        const newScrollDirection = isScrollingUp(currentScrollYPosition)
+          ? ScrollDirection.down
+          : ScrollDirection.up
+        setScrollDir(newScrollDirection)
+        previousScrollYPosition =
+          currentScrollYPosition > 0 ? currentScrollYPosition : 0
+      }
+    }
+
+    const onScroll = () => window.requestAnimationFrame(updateScrollDirection)
+
+    window.addEventListener('scroll', onScroll)
+
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [open])
+
+  useEffect(() => {
+    console.log(scrollDir)
+  }, [scrollDir])
+
   return (
     <>
       <nav className="z-20">
         <div
           className={classNames(
-            open ? 'left-36' : 'left-4',
-            'fixed top-4 z-30 transition-all duration-700 ease-in-out'
+            open
+              ? 'left-36 top-4'
+              : scrollDir === 'up'
+              ? 'left-4 top-4'
+              : '-top-16 left-4',
+            'fixed z-30 scroll-mt-8 transition-all duration-700 ease-in-out'
           )}
         >
           {/* Mobile menu button*/}
@@ -65,8 +119,25 @@ export default function Navigation() {
                 {item.name}
               </Link>
             ))}
-            <div className="font-antic">Your developer,</div>
-            <div className="font-caveat">Kayla</div>
+            <div className="flex flex-col pb-4">
+              <div className="font-antic">Your developer,</div>
+              <div className="-rotate-6 font-caveat text-2xl">Kayla</div>
+            </div>
+          </div>
+          <div className="flex justify-center space-x-6 md:order-2">
+            {socials.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-purple-300 hover:text-purple-400"
+              >
+                <span className="sr-only">{item.name}</span>
+                <item.icon
+                  className="h-6 w-6 transition duration-300 ease-in-out hover:scale-125"
+                  aria-hidden="true"
+                />
+              </a>
+            ))}
           </div>
         </div>
       </nav>
